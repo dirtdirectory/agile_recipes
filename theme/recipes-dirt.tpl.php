6bd28cdf1c6c1f773f84006ea93b0c665403d9ff
Quinn Dombrowski <quinnd@berkeley.edu>
2015-07-30 14:53:19 -0700

    code used on existing methodi.ca site

---
+++ theme/recipes-dirt.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Template for links.
+ */
+?>
+<div>
+  <p class ="dirt_title"><?php print $title; ?></p>
+  <p class ="dirt_description"><?php print $description; ?></p>
+  <p class ="dirt_description"><?php print $link; ?></p>
+</div>
